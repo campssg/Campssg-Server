56a3ed2eb815e173a3ee0aceba56c546d63ea76d
hye0e <nanda5050@naver.com>
2022-02-13 17:04:24 +0900

    Initial commit

---
+++ src/test/java/com/campssg/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.campssg.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
